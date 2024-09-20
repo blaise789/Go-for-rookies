c04454a9e04563f2dfc8784ebb27c9e0ca0e4531
blaise <blaise@kali>
2024-09-20 07:19:48 +0100

    closures done

---
+++ part2.go
@@ -0,0 +1,19 @@
+package main
+
+//  closures
+// import "fmt"
+// func counter()func() int{
+//  count:=0
+
+// 	return func() int {
+// count++
+// return count
+// 	}
+// }
+// func main(){
+// increment:=counter()
+// fmt.Println(increment())
+
+// fmt.Println(increment())
+
+// }
