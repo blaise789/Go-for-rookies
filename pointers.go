68ee8f4fb235bd230f10986a3c78cd932375c8d7
blaise <blaise@kali>
2024-09-20 16:37:36 +0100

    done with pointers

--- pointers.go
+++ pointers.go
@@ -1,20 +1,19 @@
-package main
+// package main
+// import "fmt"
 
-import "fmt"
-
-func zeroVal(ival int) {
-	ival = 0
+// func zeroVal(ival int) {
+// 	ival = 0
 	
 
-}
-func zeroPtr(iptr *int)  {
-	*iptr = 0 
+// }
+// func zeroPtr(iptr *int)  {
+// 	*iptr = 0 
 	
-}
-func main() {
-	a := 1
-	zeroVal(a)
-	fmt.Println(a)
-	zeroPtr(&a)
-    fmt.Println(a)
-}
+// }
+// func main() {
+// 	a := 1
+// 	zeroVal(a)
+// 	fmt.Println(a)
+// 	zeroPtr(&a)
+//     fmt.Println(a)
+// }
---
+++ string-runes.go
@@ -0,0 +1,24 @@
+// package main
+
+
+
+// import (
+// 	"fmt"
+// 	"unicode/utf8"
+// )
+// func main(){
+// 	a :="blaise"
+// 	fmt.Println(a[1])
+// 	fmt.Println(a)
+// 	for i:=0;i<len(a);i++{
+// 		fmt.Println(a[i])
+// 	}
+// 	    for idx, runeValue := range a{
+//         fmt.Printf("%#U starts at %d\n", runeValue, idx)
+//     }
+
+// 	fmt.Println()
+// 	fmt.Println("Rune count:", utf8.RuneCountInString(a))
+
+	
+// }
